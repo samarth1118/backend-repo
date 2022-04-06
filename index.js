9faab589623c404f5d13be296fe38f8bb6baefb2
samarth0718 <samarth0718@gmail.com>
2022-04-06 14:46:42 +0400

    Sample Hello route module created

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const helloWorld = require('./routes/hello-world')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
+app.use('/hello', helloWorld);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
